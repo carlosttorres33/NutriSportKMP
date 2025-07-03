2904fd9dbb3a682d6e578b6843bdc422023fd0c2
Carlos Torres <kisamehatake33@comunidad.unam.mx>
2025-07-03 15:32:56 -0600

    Update IOS App and Message Colors

--- feature/auth/src/commonMain/kotlin/com/nutrisport/auth/AuthScreen.kt
+++ feature/auth/src/commonMain/kotlin/com/nutrisport/auth/AuthScreen.kt
@@ -25,8 +25,11 @@ import com.nutrisport.shared.Alpha
 import com.nutrisport.shared.BebasNeueFont
 import com.nutrisport.shared.FontSize
 import com.nutrisport.shared.Surface
+import com.nutrisport.shared.SurfaceBrand
+import com.nutrisport.shared.SurfaceError
 import com.nutrisport.shared.TextPrimary
 import com.nutrisport.shared.TextSecondary
+import com.nutrisport.shared.TextWhite
 import rememberMessageBarState
 
 @Composable
@@ -47,7 +50,11 @@ fun AuthScreen() {
                 ),
             messageBarState = messageBarState,
             errorMaxLines = 2,
-            contentBackgroundColor = Surface
+            contentBackgroundColor = Surface,
+            errorContainerColor =  SurfaceError,
+            errorContentColor = TextWhite,
+            successContainerColor = SurfaceBrand,
+            successContentColor = TextPrimary
 
         ) {
             Column(
