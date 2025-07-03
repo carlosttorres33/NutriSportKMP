31e5b69d33d91681edaa20b27401c55d474d5b15
Carlos Torres <kisamehatake33@comunidad.unam.mx>
2025-07-03 17:19:24 -0600

    Koin initialization

--- composeApp/src/iosMain/kotlin/org/carlosttorres/nutrisport/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/carlosttorres/nutrisport/MainViewController.kt
@@ -1,5 +1,10 @@
 package org.carlosttorres.nutrisport
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.nutrisport.di.initializeKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        initializeKoin()
+    }
+) { App() }
