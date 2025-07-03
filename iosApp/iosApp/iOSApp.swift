0bbc2b45f8028b46bc18ff11c238898d50d493f7
Carlos Torres <kisamehatake33@comunidad.unam.mx>
2025-07-03 15:32:56 -0600

    Update IOS App and Message Colors

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -29,21 +29,4 @@ class AppDelegate : NSObject, UIApplicationDelegate {
         return true
     }
     
-    func application(
-        _ app : UIApplication,
-        open url : URL, options : [UIApplication.OpenURLOptionsKey : Any] = [:]
-    ) -> Bool {
-        
-        var handled : Bool
-        
-        handled = GIDSignIn.sharedInstance.handle(url)
-        
-        if handled {
-            return true
-        }
-        
-        return false
-        
-    }
-    
 }
--- shared/src/commonMain/kotlin/com/nutrisport/shared/Colors.kt
+++ shared/src/commonMain/kotlin/com/nutrisport/shared/Colors.kt
@@ -15,6 +15,8 @@ val Red = Color(0xFFDD0000)
 val Surface = White
 val SurfaceLighter = GrayLighter
 val SurfaceDarker = Gray
+val SurfaceBrand = Yellowish
+val SurfaceError = Red
 
 val TextPrimary = Black
 val TextSecondary = Orange
